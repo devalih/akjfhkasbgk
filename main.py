e7d303e7b6dff23c5d3654edd15a4afa7bf26bbe
ali <devalih@gmail.com>
2018-03-01 22:02:18 +0200

    Random

--- main.py
+++ main.py
@@ -1,57 +1,31 @@
-from __future__ import division
-
-import operator
-
-def from_file(file_obj):
-    line_iter = iter(file_obj)
-
-    counts_line = next(line_iter).strip()
-
-    videos_line = next(line_iter).strip()
-    obj.videos = tuple(
-        Video(id=index, size=int(size_mb), possible_caches={})
-        for index, size_mb in enumerate(videos_line.split(' '))
-    )
-
-    obj.caches = tuple(
-        Cache(id=index, size=obj.cache_size_mb, stored_videos={}, endpoints={}, possible_videos={},
-              sorted_possible_videos=[], done=False, first_time=True)
-        for index in range(obj.caches_count)
-    )
-
-    for endpoint_id in range(obj.endpoints_count):
-        endpoint_desc_line = next(line_iter).strip()
-        dc_latency, caches_count = map(int, endpoint_desc_line.split(' '))
-        endpoint = Endpoint(id=endpoint_id, dc_latency=dc_latency, caches_count=caches_count, caches_latencies={})
-        obj.endpoints.append(endpoint)
-
-        for cache in range(caches_count):
-            cache_desc_line = next(line_iter).strip()
-            cache_id, latency = map(int, cache_desc_line.split(' '))
-            endpoint.caches_latencies[cache_id] = latency
-            obj.caches[cache_id].endpoints[endpoint_id] = latency
-
-    obj.process_requests(line_iter)
-
-    return obj
-
-
+import random
 from sys import argv
-filename = argv[1]
-with open(filename) as file_obj:
-    print 'Start', filename
-    w = from_file(file_obj)
-    # w.process_requests()
-    count = 1
-    print "************** Processing all #", count, "**************\nProcessing Cache # ",
-    while not w.process_caches():
-        print
-        print "Done Processing all #", count
-        print "Caches remaining size",
-        for cache in w.caches:
-            print "Full" if cache.done else cache.size,
-        count += 1
-        print
-        print "************** Processing all #", count, "**************\nProcessing Cache # ",
-    w.output_result(filename.replace('.in', '.out'))
-    print 'Done', filename
+for filename in argv[1:]:
+    with open(filename) as file_obj:
+        line = file_obj.readline()
+        rows, cols, veh, rides, bonus, step = [int(n) for n in line.split()]
+        rides_list = []
+        for r in range(rides):
+            line = file_obj.readline()
+            rides_list.append(list([int(n) for n in line.split()]))
+        print(rides_list)
+        print()
+        print()
+
+        random_rides_list = random.sample(range(0, rides), rides)
+        print(random_rides_list)
+        rides_per_car = int(rides/veh)
+
+        with open(filename.replace('.in', '.out'), 'w') as output:
+            i = 1
+            index = 0
+            text = ""
+            while i <= veh:
+                text = str(rides_per_car) + " "
+                for r in range(rides_per_car):
+                    text += str(random_rides_list[index]) + " "
+                    index += 1
+                i += 1
+                output.write(text)
+                output.write("\n")
+                print(text)
